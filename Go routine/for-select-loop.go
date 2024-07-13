5ae79f9e7a019b8860ac60c52e4f118f44b21efb
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-13 07:01:24 +0100

    initial commit

--- Go routine/for-select-loop.go
+++ Go routine/for-select-loop.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// The select statement listens on multiple channels
+//  and performs actions based on which channel receives a message.
+
 func main() {
 
 	channel := make(chan string, 3) //buffered channel
