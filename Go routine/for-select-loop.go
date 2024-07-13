fc818d6f84d1f88c772729ddf4d75ff3f40bd500
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-13 07:04:16 +0100

    initial commit

--- Go routine/for-select-loop.go
+++ Go routine/for-select-loop.go
@@ -23,3 +23,28 @@ func main() {
 		fmt.Println(result)
 	}
 }
+
+// ==================== EXAMPLE OF A CHANNEL WITH MULTIPLE CASES ====================
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.Send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
